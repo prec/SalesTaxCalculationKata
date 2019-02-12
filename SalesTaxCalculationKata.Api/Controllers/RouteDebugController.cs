31113f9d64170cd519c59f0a0f9eac6f24c2e6d8
Jonas Daniel Dawson <dawson.jonas@muzematik.com>
2019-02-12 11:02:15 -0500

    Add RouteDebugController to help with configuring attribute routing

---
+++ SalesTaxCalculationKata.Api/Controllers/RouteDebugController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace SalesTaxCalculationKata.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteDebugController : ControllerBase
+    {
+        private readonly IActionDescriptorCollectionProvider _actionDescriptorCollectionProvider;
+
+        public RouteDebugController(IActionDescriptorCollectionProvider actionDescriptorCollectionProvider)
+        {
+            this._actionDescriptorCollectionProvider = actionDescriptorCollectionProvider;
+        }
+
+        // GET: api/RouteDebug
+        [HttpGet]
+        [HttpPut]
+        public IActionResult Get()
+        {
+            var routes = _actionDescriptorCollectionProvider.ActionDescriptors.Items.Select(x => new {
+                Action = x.RouteValues["Action"],
+                Controller = x.RouteValues["Controller"],
+                Name = x.AttributeRouteInfo?.Name,
+                Template = x.AttributeRouteInfo?.Template,
+                Contraint = x.ActionConstraints
+            }).ToList();
+            return Ok(routes);
+        }
+    }
+}
